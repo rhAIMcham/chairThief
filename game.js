8c8106b24494485ace8263c4800a8371d61a7c40
rhAIMcham <ros.hooper@aim.com.au>
2025-07-15 03:41:03 +0000

    initial game code

---
+++ game.js
@@ -0,0 +1,147 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+    state = {}
+    showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
+
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
+}
+
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option){
+    const nextTextNodeId = typeof option.nextText === "function"
+    ? option.nextText()
+    : option.nextText;
+
+    if (nextTextNodeId <= 0) {
+        return startGame();
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId);
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'first text',
+        options: [
+            {
+                text: 'option1',
+                nextText: () => [1, 2, 3][Math.floor(Math.random() * 3)]
+            },
+            {
+                text: 'option2',
+                nextText: () => [4, 5, 6][Math.floor(Math.random() * 3)]
+            },
+            {
+                text: 'option3',
+                nextText: () => [7, 8, 9][Math.floor(Math.random() * 3)]
+            },
+        ]
+    },
+    {
+        id: 2,
+        text: 'second text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: 'third text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: 'fourth text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: 'fifth text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 6,
+        text: 'sixth text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 7,
+        text: 'seventh text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 8,
+        text: 'eighth text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 9,
+        text: 'ninth text',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+]
+
+startGame()
